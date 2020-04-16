59300207e90ceae033a5fa28358f9f1924fc20b3
Jack Munkonge <jackmunkonge@gmail.com>
2020-04-16 12:28:10 +0100

    EM-2808: Add mocks to documentTaskCallbackProcessorTest

--- src/test/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskCallbackProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskCallbackProcessorTest.java
@@ -3,8 +3,12 @@ package uk.gov.hmcts.reform.em.stitching.batch;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,19 +16,48 @@ import uk.gov.hmcts.reform.em.stitching.Application;
 import uk.gov.hmcts.reform.em.stitching.domain.Callback;
 import uk.gov.hmcts.reform.em.stitching.domain.DocumentTask;
 import uk.gov.hmcts.reform.em.stitching.domain.enumeration.CallbackState;
+import uk.gov.hmcts.reform.em.stitching.domain.enumeration.TaskState;
+import uk.gov.hmcts.reform.em.stitching.service.dto.CallbackDto;
+import uk.gov.hmcts.reform.em.stitching.service.dto.DocumentTaskDTO;
 import uk.gov.hmcts.reform.em.stitching.service.mapper.DocumentTaskMapper;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class DocumentTaskCallbackProcessorTest {
 
+    @InjectMocks
     DocumentTaskCallbackProcessor documentTaskCallbackProcessor;
 
     @Autowired
+    ObjectMapper objectMapper;
+
+    @Mock
     private DocumentTaskMapper documentTaskMapper;
 
-    @Autowired
-    ObjectMapper objectMapper;
+    @Mock
+    private DocumentTaskDTO documentTaskDTOMock;
+
+    @Mock
+    private CallbackDto callbackDTOMock;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        callbackDTOMock = new CallbackDto();
+        callbackDTOMock.setCallbackUrl("https://mycallback.com");
+        callbackDTOMock.setCallbackState(CallbackState.NEW);
+
+        documentTaskDTOMock = new DocumentTaskDTO();
+        documentTaskDTOMock.setTaskState(TaskState.NEW);
+        documentTaskDTOMock.setCallback(callbackDTOMock);
+        documentTaskDTOMock.setJwt("jwt");
+
+        when(documentTaskMapper.toDto(any(DocumentTask.class))).thenReturn(documentTaskDTOMock);
+    }
 
     @Test
     public void testCallback200() {
@@ -33,7 +66,7 @@ public class DocumentTaskCallbackProcessorTest {
 
         DocumentTask documentTask = new DocumentTask();
         documentTask.setJwt("jwt");
-        uk.gov.hmcts.reform.em.stitching.domain.Callback callback = new Callback();
+        Callback callback = new Callback();
         documentTask.setCallback(callback);
         callback.setCallbackUrl("https://mycallback.com");
 
@@ -51,7 +84,7 @@ public class DocumentTaskCallbackProcessorTest {
 
         DocumentTask documentTask = new DocumentTask();
         documentTask.setJwt("jwt");
-        uk.gov.hmcts.reform.em.stitching.domain.Callback callback = new Callback();
+        Callback callback = new Callback();
         documentTask.setCallback(callback);
         callback.setCallbackUrl("https://mycallback.com");
 
